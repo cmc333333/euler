365df317c00fa9217c5c40ebd1c432d7ee0a627e
CM Lubinski <cm.lubinski@gmail.com>
2015-10-11 23:24:10 -0400

    Find max four elements in matrix, solving problem 11

--- euler_0011/src/main.rs
+++ euler_0011/src/main.rs
@@ -1,13 +1,76 @@
 use std::io;
 use std::io::{Read};
 
+fn max_horiz(matrix: &Vec<Vec<u64>>, size: usize) -> u64 {
+    let mut max = 0u64;
+    for row in matrix {
+        for i in 0..(row.len() - size + 1) {
+            let product = (0..size).fold(1, |prod, next| prod * row[i + next]);
+            if product > max {
+                max = product;
+            }
+        }
+    }
+    max
+}
+
+fn max_vert(matrix: &Vec<Vec<u64>>, size: usize) -> u64 {
+    let mut max = 0u64;
+    for (row_idx, row) in matrix.iter().enumerate() {
+        if row_idx < matrix.len() - size {
+            for col_idx in 0..row.len() {
+                let product = (0..size).fold(
+                    1, |prod, next| prod * matrix[next + row_idx][col_idx]);
+                if product > max {
+                    max = product;
+                }
+            }
+        }
+    }
+    max
+}
+
+fn max_desc(matrix: &Vec<Vec<u64>>, size: usize) -> u64 {
+    let mut max = 0u64;
+    for (row_idx, row) in matrix.iter().enumerate() {
+        for (col_idx, col) in row.iter().enumerate() {
+            if row_idx < matrix.len() - size
+                && col_idx < row.len() - size {
+                let product = (0..size).fold(
+                    1, |prod, next| prod * matrix[next + row_idx][next + col_idx]);
+                if product > max {
+                    max = product;
+                }
+            }
+        }
+    }
+    max
+}
+
+fn max_asc(matrix: &Vec<Vec<u64>>, size: usize) -> u64 {
+    let mut max = 0u64;
+    for (row_idx, row) in matrix.iter().enumerate() {
+        for (col_idx, col) in row.iter().enumerate() {
+            if row_idx >= size - 1 && col_idx < row.len() - size {
+                let product = (0..size).fold(
+                    1, |prod, next| prod * matrix[row_idx - next][next + col_idx]);
+                if product > max {
+                    max = product;
+                }
+            }
+        }
+    }
+    max
+}
+
 fn main() {
     let mut string = String::new();
     match io::stdin().read_to_string(&mut string) {
         Err(_) => println!("Problem reading from stdin"),
         Ok(_) => {
+            let problem = string.trim();
             let mut matrix: Vec<Vec<u64>> = Vec::new();
-            for row_str in string.split('\n') {
+            for row_str in problem.split('\n') {
                 matrix.push(Vec::new());
                 for col_str in row_str.split_whitespace() {
                     match (matrix.last_mut(), col_str.parse::<u64>().ok()) {
@@ -15,7 +78,11 @@ fn main() {
                         _ => ()
                     }
                 }
-            }
+            };
+            println!("{}", max_horiz(&matrix, 4));
+            println!("{}", max_vert(&matrix, 4));
+            println!("{}", max_asc(&matrix, 4));
+            println!("{}", max_desc(&matrix, 4));
         },
     }
 }
